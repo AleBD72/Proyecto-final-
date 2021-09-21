30646681f2cf459d3c835a1cb1fc748c7e157cf8
AleBD72 <camimiercadena@gmail.com>
2021-09-20 21:43:33 -0500

    Archivos para el programa de gestión de notas en C++, grupo 4

--- PROYECTO/logear.h
+++
@@ -1,5 +0,0 @@
-int menu_principal();
-void cargar (tDocentes &docentes);
-bool logear (tDocentes &docentes, int &posicion);
-void ingresar_notas (tDocentes &docentes, tLista lista [], tNotas notas [], int& cont, int cont2, int& posicion);
-void registrar_docente(tLogin &login, tDocentes &docentes);
